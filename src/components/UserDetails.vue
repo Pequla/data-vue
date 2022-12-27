00d7a12f6b73494d12555793ececf6c66189e965
Petar Kresoja <petarkresoja5@gmail.com>
2022-12-27 03:01:15 +0100

    Add qr code in user details

--- src/components/UserDetails.vue
+++ src/components/UserDetails.vue
@@ -45,6 +45,29 @@
                         <img :src="'https://visage.surgeplay.com/full/' + user.uuid" :alt="user.name" />
                     </th>
                 </tr>
+                <tr>
+                    <th>QRCODE (Andorid App)</th>
+                    <th>
+                        <!-- <QRCodeVue3 :width="200" :height="200" :value="{ type: 'cache', id: user.id }"
+                            :qrOptions="{ typeNumber: 0, mode: 'Byte', errorCorrectionLevel: 'H' }"
+                            :imageOptions="{ hideBackgroundDots: true, imageSize: 0.4, margin: 0 }" :dotsOptions="{
+                                type: 'dots',
+                                color: '#26249a',
+                                gradient: {
+                                    type: 'linear',
+                                    rotation: 0,
+                                    colorStops: [
+                                        { offset: 0, color: '#26249a' },
+                                        { offset: 1, color: '#26249a' },
+                                    ],
+                                },
+                            }" :backgroundOptions="{ color: '#ffffff' }"
+                            :cornersSquareOptions="{ type: 'dot', color: '#000000' }"
+                            :cornersDotOptions="{ type: undefined, color: '#000000' }" myclass="my-qur"
+                            imgclass="img-qr" /> -->
+                            <q-r-code-vue3 :value="qrcodeData" :width="150" :height="150"></q-r-code-vue3>
+                    </th>
+                </tr>
             </tbody>
         </table>
     </div>
@@ -55,10 +78,16 @@
 
 <script setup>
 import DateService from '@/services/DateService';
+import QRCodeVue3 from 'qrcode-vue3';
 import { toRefs } from 'vue'
 
 const props = defineProps({
     user: Object
 });
 const { user } = toRefs(props);
+
+const qrcodeData = JSON.stringify({
+    type: "cache",
+    id: user.value.id
+})
 </script>
