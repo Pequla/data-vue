b9e43a93e2f15ffea9f17792abb391f897d81ac4
Petar Kresoja <petarkresoja5@gmail.com>
2023-01-07 22:37:18 +0100

    Add meta tags

--- src/main.js
+++ src/main.js
@@ -4,5 +4,7 @@ import router from './router'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap';
+import 'highlight.js/styles/default.css'
+import 'highlight.js/lib/core'
 
 createApp(App).use(router).mount('#app')
--- src/services/DateService.js
+++ src/services/DateService.js
@@ -1,7 +1,6 @@
 export default {
     formatDate(timestamp) {
         const date = new Date(timestamp)
-        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
         return `${date.toLocaleDateString('sr-SR')} ${this.addLeadingZeros(date.getHours())}:${this.addLeadingZeros(date.getMinutes())}`
     },
     addLeadingZeros(num) {
